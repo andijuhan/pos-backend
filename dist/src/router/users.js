20e3dbf82052bf751c0ed3d6e1f3e4afd2b7a4ff
andijuhan <80870917+andijuhan@users.noreply.github.com>
2023-05-27 22:15:29 +0700

    update

--- dist/src/router/users.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const users_1 = require("../controller/users");
-const middlewares_1 = require("../middlewares");
-exports.default = (router) => {
-    router.get('/users', middlewares_1.isAuthenticated, users_1.getAllUsers);
-};
-//# sourceMappingURL=users.js.map
